bbe4c88e77601a5b339a37985d8ff39a1250ec61
Aman <107612616+21BCS5748@users.noreply.github.com>
2024-07-26 13:05:27 +0000

    Data Types

---
+++ 01_BASICS/02_dataTypes.js
@@ -0,0 +1,30 @@
+"Use strict" //Treat all JS as newer version
+
+ //alert(3+3) we are using node JS not browser
+console.log(1978)
+              //Code readability should be high
+console.log("Aman")
+
+
+let name = "Aman"  //string data type
+let age = 20       //number data type
+// let placed = yes   //boolean data type
+
+
+// number data type Range 2^53
+// bigint for large numbers
+// string in " "quotes
+// boolean true/false
+// null is a standalone value
+// undefined 
+// symbol is for uniqueness
+
+//Object
+
+console.log(typeof undefined) // undefined data type
+console.log(typeof null) // object data type
+
+
+// Documentation
+// https://tc39.es/ecma262/
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript
